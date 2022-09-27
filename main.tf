27355ee83a86a7b7bcfa2830f9c5728babe99225
periwinkleFTW <29137960+periwinkleFTW@users.noreply.github.com>
2022-09-26 22:53:08 -0600

    working trigger

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "google_cloudbuild_trigger" "filename-trigger" {
+resource "google_cloudbuild_trigger" "my-trigger" {
   name = "tf-trigger"
 
   service_account = "projects/tf-test-357717/serviceAccounts/tf-srv-account@tf-test-357717.iam.gserviceaccount.com"
@@ -6,7 +6,13 @@ resource "google_cloudbuild_trigger" "filename-trigger" {
   trigger_template {
     branch_name = "main"
     repo_name   = "github_periwinkleftw_cf-test"
+
+    # Trigger build after the push to main branch
+    push {
+      branch = "^main$"
+    }
   }
+  ignored_files = [".gitignore"]
 
   filename = "cloudbuild.yaml"
 }
